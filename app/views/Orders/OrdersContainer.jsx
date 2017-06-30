12ea5cad7d4364bd518f1a8aeb33ea6741f0cdd6
samskinnerphd <samskinnerphd@gmail.com>
2017-06-30 12:36:11 -0400

    set up Orders

--- app/views/Orders/OrdersContainer.jsx
+++ app/views/Orders/OrdersContainer.jsx
@@ -10,3 +10,46 @@
  *
  * @exports OrdersContainer
  */
+// Module imports
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+
+// Component imports
+import OrdersCreateContainer from './Create/OrdersCreateContainer';
+import OrdersIndexContainer from './Index/OrdersIndexContainer';
+import OrdersShowContainer from './Show/OrdersShowContainer';
+
+
+/**
+ * @class OrdersContainer
+ * @description Routing container for pertinent Orders routes
+ *   "/orders/create" exact Route renders the OrdersCreateContainer
+ *   "/orders/:id" exact Route renders the OrdersShowContainer
+ *   "/orders" Route renders the OrdersIndexContainer
+ */
+export default class OrdersContainer extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route
+          exact
+          path="/orders/create/:customer_id"
+          component={OrdersCreateContainer}
+        />
+        <Route
+          exact
+          path="/orders/:id"
+          component={OrdersShowContainer}
+        />
+        <Route
+          path="/orders"
+          component={OrdersIndexContainer}
+        />
+      </Switch>
+    );
+  }
+}
+
+OrdersContainer.propTypes = {};
+OrdersContainer.defaultProps = {};
--- app/views/Routes.jsx
+++ app/views/Routes.jsx
@@ -18,6 +18,7 @@ import {
 
 // Component imports
 import CustomersContainer from './Customers/CustomersContainer';
+import OrdersContainer from './Orders/OrdersContainer';
 import notProtectedHOC from './common/hoc/notProtected';
 import protectedHOC from './common/hoc/protected';
 import UnauthenticatedContainer from './Unauthenticated/UnauthenticatedContainer';
@@ -50,6 +51,10 @@ export default class Routes extends Component {
             <Route
               path="/customers"
               component={protectedHOC(CustomersContainer)}
+            />
+           <Route
+              path="/orders"
+              component={protectedHOC(OrdersContainer)}
             />
             <Route
               path="/"
