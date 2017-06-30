4fbf4405f35162d4bdf006f494410c105f581285
samskinnerphd <samskinnerphd@gmail.com>
2017-06-30 12:34:26 -0400

    add Orders/Index

--- app/views/Orders/Index/actions/ordersIndexActions.jsx
+++ app/views/Orders/Index/actions/ordersIndexActions.jsx
@@ -5,3 +5,72 @@
  * @description Redux actions. Contains sync action-creators and async actions using Redux-Thunk.
  *
  */
+// Custom imports
+import { get } from '../../../../utilities/apiUtilities';
+
+export const REQUEST_ORDERS_INDEX_ACTION = 'REQUEST_ORDERS_INDEX_ACTION';
+export const RECEIVE_ORDERS_INDEX_SUCCESS = 'RECEIVE_ORDERS_INDEX_SUCCESS';
+export const RECEIVE_ORDERS_INDEX_FAILURE = 'RECEIVE_ORDERS_INDEX_FAILURE';
+
+/**
+ * @function requestOrdersIndexAction
+ * @description Action-creator
+ */
+const requestOrdersIndexAction = () => ({
+  type: REQUEST_ORDERS_INDEX_ACTION,
+});
+
+/**
+ * @function receiveOrdersIndexSuccess
+ * @description Action-creator
+ * @param orders
+ * @param pagination
+ */
+const receiveOrdersIndexSuccess = (orders, pagination) => ({
+  type: RECEIVE_ORDERS_INDEX_SUCCESS,
+  payload: {
+    orders,
+    pagination,
+  },
+});
+
+/**
+ * @function receiveOrdersIndexFailure
+ * @description Action-creator
+ * @param error
+ */
+const receiveOrdersIndexFailure = error => ({
+  type: RECEIVE_ORDERS_INDEX_FAILURE,
+  payload: {
+    error,
+  },
+});
+
+/**
+ * @function performOrdersIndexSearch
+ * @description GET /orders for retrieving Orders
+ * @param page
+ * @param searchTerm
+ * @param customer_id
+ */
+export const performOrdersIndexSearch = (page = 1, searchTerm, customer_id) =>
+  (dispatch) => {
+    dispatch(requestOrdersIndexAction());
+    const basePath = `/orders?page=${page}`;
+    const enhancedpath = searchTerm ? `${basePath}&searchTerm=${searchTerm}` : basePath;
+    const path = customer_id ? `${enhancedPath}&customer_id=${customer_id}` : enhancedPath;
+    get(path)
+      .then((response) => {
+        const pagination = {
+          total: response.total,
+          limit: response.limit,
+          page: response.page,
+          pages: response.pages,
+        };
+
+        dispatch(receiveOrdersIndexSuccess(response.docs, pagination));
+      })
+      .catch((e) => {
+        dispatch(receiveOrdersIndexFailure(e));
+      });
+  };
--- app/views/Orders/Index/reducers/ordersIndexReducer.jsx
+++ app/views/Orders/Index/reducers/ordersIndexReducer.jsx
@@ -20,3 +20,56 @@
  * };
  *
  */
+// Type imports
+import {
+  REQUEST_ORDERS_INDEX_ACTION,
+  RECEIVE_ORDERS_INDEX_SUCCESS,
+  RECEIVE_ORDERS_INDEX_FAILURE,
+} from '../actions/ordersIndexActions';
+
+
+export const initialState = {
+  isFetching: false,
+  orders: [],
+  pagination: {
+    total: 0,
+    limit: 10,
+    page: 1,
+    pages: 1,
+  },
+  error: null,
+};
+
+
+/**
+ * @description Reducer for ordersIndex
+ * @param reduxState
+ * @param action
+ * @returns {*}
+ */
+export default (reduxState = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case REQUEST_ORDERS_INDEX_ACTION:
+      return {
+        ...reduxState,
+        isFetching: true,
+        error: null,
+      };
+    case RECEIVE_ORDERS_INDEX_SUCCESS:
+      return {
+        ...reduxState,
+        isFetching: false,
+        orders: payload.orders,
+        pagination: payload.pagination,
+      };
+    case RECEIVE_ORDERS_INDEX_FAILURE:
+      return {
+        ...reduxState,
+        isFetching: false,
+        error: payload.error,
+      };
+    default:
+      return reduxState;
+  }
+};
