ff1d048f994dd95372a18601abe77808fd891a19
samskinnerphd <samskinnerphd@gmail.com>
2017-06-30 12:33:40 -0400

    add Orders/Create

--- app/views/Orders/Create/actions/ordersCreateActions.jsx
+++ app/views/Orders/Create/actions/ordersCreateActions.jsx
@@ -5,3 +5,37 @@
  * @description Redux actions. Contains sync action-creators and async actions using Redux-Thunk.
  *
  */
+import  { history } from '../../../Routes'
+import { post } from '../../../../utilities/apiUtilities'
+
+export const REQUEST_ORDERS_CREATE_ACTION = 'REQUEST_ORDERS_CREATE_ACTION'
+export const RECEIVE_ORDERS_CREATE_SUCCESS = 'RECEIVE_ORDERS_CREATE_SUCCESS'
+export const RECEIVE_ORDERS_CREATE_FAILURE = 'RECEIVE_ORDERS_CREATE_FAILURE'
+
+const requestOrdersCreateAction = () => ({
+  type: REQUEST_ORDERS_CREATE_ACTION,
+})
+
+const receiveOrdersCreateSuccess = () => ({
+  type: RECEIVE_ORDERS_CREATE_SUCCESS,
+})
+
+const receiveOrdersCreateFailure = () => ({
+  type: RECEIVE_ORDERS_CREATE_FAILURE,
+  payload: {
+    error,
+  },
+})
+
+export const performOrdersCreateAction = params =>
+  (dispatch) => {
+    dispatch(requestOrdersCreateAction())
+    post('./orders', params)
+      .then(() => {
+        dispatch(receiveOrdersCreateSuccess())
+        history.push('./orders')
+        })
+      .catch((e) => {
+        dispatch(recieveOrdersCreateFailure(e))
+        })
+  }
--- app/views/Orders/Create/reducers/ordersCreateReducer.jsx
+++ app/views/Orders/Create/reducers/ordersCreateReducer.jsx
@@ -13,3 +13,47 @@
  * };
  *
  */
+// Type imports
+import {
+  REQUEST_ORDERS_CREATE_ACTION,
+  RECEIVE_ORDERS_CREATE_SUCCESS,
+  RECEIVE_ORDERS_CREATE_FAILURE,
+} from '../actions/ordersCreateActions';
+
+
+export const initialState = {
+  isFetching: false,
+  error: null,
+};
+
+
+/**
+ * @description Reducer for customersCreate
+ * @param reduxState
+ * @param action
+ * @returns {*}
+ */
+export default (reduxState = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case REQUEST_ORDERS_CREATE_ACTION:
+      return {
+        ...reduxState,
+        isFetching: true,
+        error: null,
+      };
+    case RECEIVE_ORDERS_CREATE_SUCCESS:
+      return {
+        ...reduxState,
+        isFetching: false,
+      };
+    case RECEIVE_ORDERS_CREATE_FAILURE:
+      return {
+        ...reduxState,
+        isFetching: false,
+        error: payload.error,
+      };
+    default:
+      return reduxState;
+  }
+};
